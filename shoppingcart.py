e87ccca8e0939e5e9471514bce24eae7a665f051
krishnamoorthy s <129971645+krishnamoorthy05@users.noreply.github.com>
2023-04-12 21:52:58 +0530

    Update shoppingcart.py

--- shoppingcart.py
+++ shoppingcart.py
@@ -1,9 +1,11 @@
 class shoppingcart :
     items = {'toys':4,'phones':8,'shirt':6,'pants':10,'shoes':10}
+    prices= {'toys':100,'phones':2500,'shirt':1000,'pants':700,'shoes':550}
     def __init__(self,c_name,ph_no):
         self.c_name=c_name
         self.ph_no=ph_no
         self.cart={}
+        self.price=0
     def main(self):
         print()
         op=int(input(' Enter 1 to display all the item : \n Enter 2 to display your details : \n Enter 3 to add items in cart : \n Enter 4 to remove items from cart : \n Enter 5 to exit : '))
@@ -29,22 +31,24 @@ class shoppingcart :
     def display(cls):
         print('Items available are : ')
         for items in shoppingcart.items:
-            print(f'{items} : {shoppingcart.items[items]}')
+            print(f'{items} : {shoppingcart.items[items]} ,price per unit : {shoppingcart.prices[items]}')
     def c_display (self):
         print(f'Name : {self.c_name}')
         print (f'Phonenumber : {self.ph_no}')
         if len(self.cart)==0:
-            print('Your cart is empty')
+            print('Your cart is empty ,The value of cart is 0')
         else :
             print("items present in your cart")
             for items in self.cart:
-                print(f'{item} : {self.cart[item]}')
+                print(f'{items} : {self.cart[items]}')
     def additems(self):
         item_name = input ("enter the item name :")
         if item_name not in shoppingcart.items:
-            prinnt("items not available")
+            print("items not available")
         else :
-            count=int(input("Enter the number of items you want to add :"))
+            count=int(input(f'Tne number of {item_name} left is {shoppingcart.items[item_name]} \nEnter the number of {item_name} you want to add :'))
+            self.price+=(shoppingcart.prices[item_name]*count)
+            print(f'your cart value is :Rs {self.price}')
             if shoppingcart.items[item_name]<count:
                 print(f'stock not available ,only {shoppingcart.items[item_name]} left : ')
             else :
@@ -58,7 +62,9 @@ class shoppingcart :
         if bool (self.cart):
             item_name=input("Enter the item name :")
             if item_name  in self.cart:
-                count=int(input("Enter the number of item to remove "))
+                count=int(input("Enter the number of item to remove:"))
+                self.price-=(shoppingcart.prices[item_name]*count)
+                print(f'your cart value is :Rs {self.price} ')
                 if self.cart[item_name]<count :
                     print("Not possible to remove")
                 else:
